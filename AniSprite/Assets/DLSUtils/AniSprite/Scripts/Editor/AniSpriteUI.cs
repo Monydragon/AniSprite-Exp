9d5571d8df07e1269712f50d76b9aecfe8c1ad23
Jeffery Miller <Mony@dragonlensstudios.com>
2018-06-30 12:34:34 -0700

    Added folder structure and added AniSpriteUI
    Added folder structure and setup namespace and added AniSpriteUI.cs

---
+++ AniSprite/Assets/DLSUtils/AniSprite/Scripts/Editor/AniSpriteUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+
+namespace DLSUtils.AniSprite.Editor
+{
+    public class AniSpriteUI : EditorWindow
+    {
+        [MenuItem("DLSUtils/AniSprite/AniSprite Editor %#a", priority = 0)]  //Control-Shift-A as hotkey
+        static void Init()
+        {
+            // Get existing open window or if none, make a new one:
+            AniSpriteUI window = (AniSpriteUI)GetWindow(typeof(AniSpriteUI));
+            window.Show();
+        }
+
+        [MenuItem("DLSUtils/AniSprite/About", priority = 3)]
+        public static void About()
+        {
+            EditorUtility.DisplayDialog("About", "AniSprite\nAn Asset by: DLSUtils\nVersion: 0.1", "Close");
+        }
+
+        // Use this for initialization
+        void Start () {
+		
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+    }
+}
