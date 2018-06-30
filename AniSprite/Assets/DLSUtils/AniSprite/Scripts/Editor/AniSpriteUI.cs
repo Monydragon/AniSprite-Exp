7baf1d12b3d85604eaeeb71dfda82bc0dae3525c
Jeffery Miller <Mony@dragonlensstudios.com>
2018-06-30 13:03:53 -0700

    Added PostProcesser.cs and Edited AniSpriteUI
    Added simple PostProcesser script and added a little more sample code to AniSpriteUI for testing.

--- AniSprite/Assets/DLSUtils/AniSprite/Scripts/Editor/AniSpriteUI.cs
+++ AniSprite/Assets/DLSUtils/AniSprite/Scripts/Editor/AniSpriteUI.cs
@@ -1,31 +1,50 @@
-﻿using UnityEditor;
+﻿using UnityEngine;
+using UnityEditor;
 
 namespace DLSUtils.AniSprite.Editor
 {
     public class AniSpriteUI : EditorWindow
     {
-        [MenuItem("DLSUtils/AniSprite/AniSprite Editor %#a", priority = 0)]  //Control-Shift-A as hotkey
+        //Control-Shift-A as hotkey
+        [MenuItem("DLSUtils/AniSprite/AniSprite Editor %#a", priority = 0)]
         static void Init()
         {
             // Get existing open window or if none, make a new one:
-            AniSpriteUI window = (AniSpriteUI)GetWindow(typeof(AniSpriteUI));
-            window.Show();
+            AniSpriteUI uiWindow = (AniSpriteUI)GetWindow(typeof(AniSpriteUI));
+            uiWindow.Show();
         }
 
+        /// <summary>
+        /// This is the About window that displays a window containing software credits and attributions.
+        /// </summary>
         [MenuItem("DLSUtils/AniSprite/About", priority = 3)]
         public static void About()
         {
-            EditorUtility.DisplayDialog("About", "AniSprite\nAn Asset by: DLSUtils\nVersion: 0.1", "Close");
+            EditorUtility.DisplayDialog("About",
+                "AniSprite\nDeveloped by: DLSUtils\nVersion: 0.1",
+                "Close");
         }
 
-        // Use this for initialization
-        void Start () {
-		
+        void OnEnable()
+        {
+
         }
-	
-        // Update is called once per frame
-        void Update () {
-		
+
+        void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+
+            GUILayout.Button("TestButton");
+            EditorGUILayout.Popup(0, new string[] { "Option 1", "Option2" });
+            
+            EditorGUILayout.EndVertical();
+        }
+
+        //This needs to be on a subclass
+
+        void OnPostprocessTexture(Texture2D texture)
+        {
+
         }
     }
 }
---
+++ AniSprite/Assets/DLSUtils/AniSprite/Scripts/Editor/PostProcessor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace DLSUtils.AniSprite.Editor
+{
+    public class PostProcessor : AssetPostprocessor
+    {
+        void OnPostprocessSprites(Texture2D texture, Sprite[] sprites)
+        {
+            Debug.Log("Got a callback on Sprites PP");
+        }
+
+        void OnPostprocessTexture(Texture2D texture)
+        {
+            Debug.Log("Got a callback on Texture PP");
+        }
+    }
+}
