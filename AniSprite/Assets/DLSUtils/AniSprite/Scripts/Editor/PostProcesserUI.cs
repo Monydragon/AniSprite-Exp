c07e2a30cace2e54ff3bf56f6d6b75faebcc1da7
bloeys <bloeys@outlook.com>
2018-07-23 00:00:17 +0200

    Working on animation system
    * Working on creating animation from the passed sprites.
    * Implemented a proof of concept that creates an animation and animation controller along with sprite keyframes to the animation.
    * Seems sprites must be loaded from resources.
    * To create an animation, valid references to the sprites are needed, however the sprites passed by the postprocessor are invalidated later so can't be used.

--- AniSprite/Assets/DLSUtils/AniSprite/Scripts/Editor/PostProcesserUI.cs
+++ AniSprite/Assets/DLSUtils/AniSprite/Scripts/Editor/PostProcesserUI.cs
@@ -8,40 +8,42 @@ namespace DLSUtils.AniSprite.Editor
     public class PostProcesserUI : EditorWindow
     {
         public static string Message;
-        public static string DebugingMessage;
+        public static string DebugingMessage = "Debug";
         public static string ButtonMessage;
 
-        public static void ShowUI(Texture2D texture, Sprite[] sprites, TextureImporter textureImporter)
+        Texture2D texture;
+        Sprite[] sprites;
+        TextureImporter textureImporter;
+
+        public void ShowUI(Texture2D texture, Sprite[] sprites, TextureImporter textureImporter)
         {
-            ButtonMessage = "Please Press Me";
-            var tempMessage = "";
-
             if (textureImporter.spriteImportMode != SpriteImportMode.Multiple)
-            { return; }
+                return;
 
-            for (var index = 0; index < sprites.Length; index++)
-            {
-                var sprite = sprites[index];
-                tempMessage += $"[{index}]{sprite.name}";
-                if (index < sprites.Length) { tempMessage += ", "; }
-            }
-
-            Message = tempMessage;
-
-            PostProcesserUI popup = CreateInstance<PostProcesserUI>();
-            popup.position = new Rect(Screen.width / 2, Screen.height / 2, 250, 300);
-            popup.ShowUtility();
+            this.texture = texture;
+            this.sprites = sprites;
+            this.textureImporter = textureImporter;
+            Show();
         }
 
         void OnGUI()
         {
+            for (var index = 0; index < sprites.Length; index++)
+            {
+                Message += $"[{index}]{sprites[index].name}";
+
+                if (index < sprites.Length)
+                    Message += ", ";
+            }
+
             EditorGUILayout.LabelField(Message, EditorStyles.wordWrappedLabel);
             GUILayout.Space(25);
-            EditorGUILayout.PrefixLabel("Debugging");
+
+            EditorGUILayout.PrefixLabel("Debugging: ");
             EditorGUILayout.LabelField(DebugingMessage, EditorStyles.wordWrappedLabel);
             GUILayout.Space(25);
 
-            if (GUILayout.Button(ButtonMessage))
+            if (GUILayout.Button("Close"))
             {
                 this.Close();
             }
--- AniSprite/Assets/DLSUtils/AniSprite/Scripts/Editor/PostProcessor.cs
+++ AniSprite/Assets/DLSUtils/AniSprite/Scripts/Editor/PostProcessor.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Animations;
 
 namespace DLSUtils.AniSprite.Editor
 {
@@ -7,7 +8,31 @@ namespace DLSUtils.AniSprite.Editor
     {
         void OnPostprocessSprites(Texture2D texture, Sprite[] sprites)
         {
-            PostProcesserUI.ShowUI(texture,sprites, assetImporter as TextureImporter);
+            //PostProcesserUI popup = ScriptableObject.CreateInstance<PostProcesserUI>();
+            //popup.ShowUI(texture, sprites, assetImporter as TextureImporter);
+
+            int wanted = sprites.Length;
+            ObjectReferenceKeyframe[] spriteKeyFrames = new ObjectReferenceKeyframe[wanted];
+            for (int i = 0; i < wanted; i++)
+            {
+                spriteKeyFrames[i] = new ObjectReferenceKeyframe
+                {
+                    time = i,
+                    value = sprites[i]
+                };
+            }
+
+            AnimationClip animClip = new AnimationClip { frameRate = 30 };
+            AnimationUtility.SetObjectReferenceCurve(animClip, new EditorCurveBinding() { path = "", propertyName = "sprite", type = typeof(SpriteRenderer) }, spriteKeyFrames);
+
+            var resourcesSprites = Resources.LoadAll<Sprite>("Test2");
+            spriteKeyFrames[0].value = resourcesSprites[0]; //Loading from resources works fine!!
+            AnimatorController ac = AnimatorController.CreateAnimatorControllerAtPathWithClip("assets/mycontroller.controller", animClip);
+
+            AssetDatabase.CreateAsset(animClip, "assets/myanim.anim");
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
         }
 
         void OnPostprocessTexture(Texture2D texture)
